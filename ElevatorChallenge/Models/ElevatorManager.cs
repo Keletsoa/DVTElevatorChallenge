fd2a81fa8fcc033c32e8a3118f62df0b09d3323f
Keletso Rafedile <10019813@arcelormittalsa.com>
2023-07-03 09:50:11 +0200

    Working on Console and Models

--- ElevatorChallenge/Models/ElevatorManager.cs
+++ ElevatorChallenge/Models/ElevatorManager.cs
@@ -1,18 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ElevatorChallenge.Models
+﻿namespace ElevatorChallenge.Models
 {
     public class ElevatorManager
     {
-        private List<Elevator> Elevators { get; set; }
+        public List<Elevator> Elevators { get; set; }
 
         public ElevatorManager(int numOfElevators, int capacity)
         {
             Elevators = new List<Elevator>();
+
+            for (int i = 1; i <= numOfElevators; i++)
+            {
+                var elevator = new Elevator(i, capacity);
+                Elevators.Add(elevator);
+            }
+        }
+
+        public Elevator GetClosestElevator(int floor)
+        {
+            var availableElevator = Elevators.Where(a => !a.IsElevatorMoving).ToList();
+
+            if (availableElevator.Count == 0)
+                return null;
+
+            var closestElevator = availableElevator[0];
+            var minDistance = Math.Abs(closestElevator.CurFloorPosition - floor);
+
+            foreach (var elevator in availableElevator)
+            {
+                var distance = Math.Abs(elevator.CurFloorPosition - floor);
+                if (distance < minDistance)
+                {
+                    closestElevator = elevator;
+                    minDistance = distance;
+                }
+            }
+
+            return closestElevator;
+        }
+
+        public void ElevatorStatus()
+        {
+            foreach (var elevator in Elevators)
+            {
+                var direction = elevator.IsElevatorMoving ? elevator.ElevatorDirection.ToString() : "Not Moving";
+                Console.WriteLine($"Elevator {elevator.Id} is at floor {elevator.CurFloorPosition}, {direction}");
+            }
+        }
+
+        public void CallElevator (int floor)
+        {
+            var elevator = GetClosestElevator(floor);
+
+            if(elevator == null)
+            {
+                Console.WriteLine("No available elevator at the moment.");
+                return;
+            }
+
+            Console.WriteLine($"Elevator {elevator.Id} is coming to floor {floor}");
+            elevator.MoveToFloor(floor);
         }
     }
 }
--- ElevatorChallenge/Program.cs
+++ ElevatorChallenge/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ElevatorChallenge.Models;
+
+//Lets create an elevator manager with 2 elevators and a capacity of 8!
+var elevatorManager = new ElevatorManager(2, 8);
+
+//Lets assume Elevator 1 is at floor 3 and Elevator 2 is at floor 5
+elevatorManager.Elevators[0].MoveToFloor(3);
+elevatorManager.Elevators[1].MoveToFloor(5);
+
+//Lets show the status of the elevators
+elevatorManager.ElevatorStatus();
+
+//Lets call an elevator to floor 4
+elevatorManager.CallElevator(4);
+
+//Lets show the status of the elevators
+elevatorManager.ElevatorStatus();
+
+Console.ReadLine();
