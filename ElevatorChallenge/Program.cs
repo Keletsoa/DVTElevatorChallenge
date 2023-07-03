775144932e08bca27950a8a41e22bcb1ed96b214
Keletso Rafedile <10019813@arcelormittalsa.com>
2023-07-03 10:35:17 +0200

    Uncommenting code

--- ElevatorChallenge/Program.cs
+++ ElevatorChallenge/Program.cs
@@ -5,16 +5,16 @@ using ElevatorChallenge.Models;
 var elevatorManager = new ElevatorManager(2, 8);
 
 //Lets assume Elevator 1 is at floor 3 and Elevator 2 is at floor 5
-//elevatorManager.Elevators[0].MoveToFloor(3);
-//elevatorManager.Elevators[1].MoveToFloor(5);
+elevatorManager.Elevators[0].MoveToFloor(3);
+elevatorManager.Elevators[1].MoveToFloor(5);
 
 //Lets show the status of the elevators
 elevatorManager.ElevatorStatus();
 
 //Lets call an elevator to floor 4
-//elevatorManager.CallElevator(4);
+elevatorManager.CallElevator(4);
 
 //Lets show the status of the elevators
-//elevatorManager.ElevatorStatus();
+elevatorManager.ElevatorStatus();
 
 Console.ReadLine();
